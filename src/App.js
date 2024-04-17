031a036f42246c5c0217f6f08a347f2997320750
Torobaev23 <daniyartorobaev23@gmail.com>
2024-04-17 17:57:00 +0600

    погода

--- src/App.js
+++ src/App.js
@@ -6,18 +6,22 @@ export default function App() {
     const [data, setData] = useState({});
     const [location, setLocation] = useState('');
 
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&lang=ru&appid=875619ba74aa91c2a603e42a5ba1ede1`;
-
     const searchLocation = (event) => {
-        if (event.key === "Enter") {
+        const trimmedLocation = location.trim();
+        if ((event.key === "Enter" && trimmedLocation !== "") || event.key.trim() === "") {
+            const url = `https://api.openweathermap.org/data/2.5/weather?q=${trimmedLocation}&units=metric&lang=ru&appid=875619ba74aa91c2a603e42a5ba1ede1`;
+            console.log(url);
             axios.get(url).then((response) => {
                 setData(response.data);
                 console.log(response.data);
+            }).catch((error) => {
+                console.error(error);
             });
             setLocation("");
-            console.log(data)
+            console.log(data);
         }
     };
+
 
     return (
         <div className="app">
@@ -30,41 +34,41 @@ export default function App() {
                     type='text'
                 />
             </div>
-           <div className='container'>
-               <div className='top'>
-                   <div className='location'>
-                       <p>{data.name}</p>
-                   </div>
-                   <div className='temp'>
-                       {data.main ? <h1>{data.main.temp.toFixed()}</h1> : null}
-                   </div>
-                   <div className='description'>
-                       {data.weather ? <p>{data.weather[0].description}</p> : null}
-                   </div>
-               </div>
-               {data.name !== undefined && (
-                   <div className='bottom'>
-                       <div className='feels'>
-                           {data.main ? (
-                               <p className='bold'>{data.main.feels_like.toFixed()}</p>
-                           ) : null}
-                           <p>Чувствуется как:</p>
-                       </div>
-                       <div className='humidity'>
-                           {data.main ? (
-                               <p className='bold'>{data.main.humidity.toFixed()}%</p>
-                           ) : null}
-                           <p>Влажность:</p>
-                       </div>
-                       <div className='wind'>
-                           {data.wind ? (
-                               <p className='bold'>{data.wind.speed.toFixed()}м/c</p>
-                           ) : null}
-                           <p>Скорость ветра:</p>
-                       </div>
-                   </div>
-               )}
-           </div>
+            <div className='container'>
+                <div className='top'>
+                    <div className='location'>
+                        <p>{data.name}</p>
+                    </div>
+                    <div className='temp'>
+                        {data.main ? <h1>{data.main.temp.toFixed()}</h1> : null}
+                    </div>
+                    <div className='description'>
+                        {data.weather ? <p>{data.weather[0].description}</p> : null}
+                    </div>
+                </div>
+                {data.name !== undefined && (
+                    <div className='bottom'>
+                        <div className='feels'>
+                            {data.main ? (
+                                <p className='bold'>{data.main.feels_like.toFixed()}</p>
+                            ) : null}
+                            <p>Чувствуется как:</p>
+                        </div>
+                        <div className='humidity'>
+                            {data.main ? (
+                                <p className='bold'>{data.main.humidity.toFixed()}%</p>
+                            ) : null}
+                            <p>Влажность:</p>
+                        </div>
+                        <div className='wind'>
+                            {data.wind ? (
+                                <p className='bold'>{data.wind.speed.toFixed()}м/c</p>
+                            ) : null}
+                            <p>Скорость ветра:</p>
+                        </div>
+                    </div>
+                )}
+            </div>
         </div>
     );
 }
